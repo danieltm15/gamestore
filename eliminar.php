ffb8f15ffab1a7dad210fc37d01df8d305635a7e
Daniel Toledo Moreno <danieltoledo790@gmail.com>
2021-10-20 12:27:12 -0500

    w

--- eliminar.php
+++ eliminar.php
@@ -1,7 +1,7 @@
 <?php
 	include 'librerias/conexion.php';
 	
-$sql = "DROP TABLE imgjuego";
+$sql = "DROP TABLE juego";
 
 if ($conn->query($sql) === TRUE) {
   echo "<script> alert('Database elimino successfully');</script>";
