f7f0930dda59b9b6dc807a852d44e2b99d3d59f9
Daniel Toledo Moreno <danieltoledo790@gmail.com>
2021-10-20 10:28:16 -0500

    f

--- librerias/conexion.php
+++ librerias/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "gameStoreBaseDatos";
+$dbhost = "us-cdbr-east-04.cleardb.com";
+$dbuser = "b64b78780e07c5";
+$dbpass = "1572a9e1";
+$dbname = "heroku_7830e8258e6913f";
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass , $dbname);
 
