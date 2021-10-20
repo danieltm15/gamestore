e021466db8aab728c42660e24e220846e626b8cc
Daniel Toledo Moreno <danieltoledo790@gmail.com>
2021-10-20 12:28:41 -0500

    f

--- eliminar.php
+++ eliminar.php
@@ -1,7 +1,15 @@
 <?php
 	include 'librerias/conexion.php';
 	
-$sql = "DROP TABLE juego";
+$sql = "create table Juego(
+	idJuego int auto_increment primary key,
+    nombre varchar(30) not null,
+    precio float,
+    descriShort varchar(600),
+    descriLong varchar(2500),
+    categoria varchar(30) not null,
+    plataforma varchar(30) not null
+)";
 
 if ($conn->query($sql) === TRUE) {
   echo "<script> alert('Database elimino successfully');</script>";
