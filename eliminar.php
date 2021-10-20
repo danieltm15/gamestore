73e4aef9ae821eef17667101361a6b1770dbfa64
Daniel Toledo Moreno <danieltoledo790@gmail.com>
2021-10-20 12:17:09 -0500

    e

--- eliminar.php
+++ eliminar.php
@@ -1,29 +1,12 @@
 <?php
 	include 'librerias/conexion.php';
 	
-	$elimi = 5;
-	
-	// sql to delete a record
-$sql = "DELETE FROM imgjuego WHERE id_Juego=".$elimi;
+$sql = "DROP TABLE imgjuego";
 
 if ($conn->query($sql) === TRUE) {
-  echo "Record deleted successfully";
+  echo "Database elimino successfully";
 } else {
-  echo "Error deleting record: " . $conn->error;
-}
-
-$conn->close();
-
-
-	include 'librerias/conexion.php';
-	
-	// sql to delete a record
-$sql = "DELETE FROM juego WHERE idJuego=".$elimi;
-
-if ($conn->query($sql) === TRUE) {
-  echo "Record deleted successfully";
-} else {
-  echo "Error deleting record: " . $conn->error;
+  echo "Error elimi database: " . $conn->error;
 }
 
 $conn->close();
